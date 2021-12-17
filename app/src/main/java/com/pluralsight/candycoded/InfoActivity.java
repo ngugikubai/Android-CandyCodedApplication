b6304f98d50761e32a9fc54a57f649904359284d
ngugikubai <kubaibrian@gmail.com>
2021-12-17 12:37:22 +0300

    Phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -46,5 +46,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
-
+    public void createPhoneIntent(View view){
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+        intent.setData(uri);
+        startActivity(intent);
+    }
 }
